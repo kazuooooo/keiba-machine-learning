a8d59b13bffc58bdcc3b1a16ad4397e8d6b5c493
kazuooooo <matsumotokazuya7@gmail.com>
2016-05-29 20:01:53 +0900

    saved better win 0529

--- keiba_neuralnetwork/try_some_theta.m
+++ keiba_neuralnetwork/try_some_theta.m
@@ -1,5 +1,5 @@
 % データを準備
-data = load('machine_learning_data20160529-03:29')
+data = load('machine_learning_data20160529-04:07')
 
 % Xのデータを取得
 X_odds = data(:, 3:18);
@@ -16,12 +16,14 @@ X = [X_odds X_dist X_course X_horce_num];
 
 % thetaの値をロード
 % theta0529-5はすごいよさそう入力はX = [X_odds X_dist X_course X_horce_num];
-load('theta0529-5')
+load('theta0529-5');
 
 % evaluate cv sam
-larger_pop_num = 5
+larger_pop_num = 5;
 % race_id = data(:, 1);
 % pですら1番人気を全く予想しないのはおかしいんじゃ？？
 [p op] = predict(Theta1, Theta2, X, larger_pop_num, X_odds)
+p_with_id = [data(:, 1:2) p]
+op_with_id = [data(:, 1:2) op]
 %[p_wins p_wins_yen p_loses p_loses_yen p_hit_persentage p_result] = calcResult(p, y, X_odds, race_id)
 %[op_wins op_wins_yen op_loses op_loses_yen op_hit_persentage op_result] = calcResult(op, y, X_odds, race_id)
